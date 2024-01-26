4c1ce52d9238eacca780df26c88c5141d14b26b5
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-01-26 17:59:17 -0500

    Exercise 2.1

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = ({text}) => {
+  console.log(text);
+  return <h1>{text}</h1>
+}
+
+const Part = ({ part, exercises }) => {
+  console.log(part, exercises);
+  return <p>{part} {exercises}</p>
+}
+
+const Content = ({ parts }) => {
+  console.log(parts);
+  return (
+    <>
+      {parts.map((part) => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+    </>
+  )
+}
+
+const Course = ({ course }) => {
+  console.log(course);
+  return (
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
